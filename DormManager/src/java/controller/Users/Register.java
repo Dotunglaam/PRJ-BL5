48f8d3ab895af5edfd49863d50dad88edb633c41
Dotunglaam <dotunglam200xx@gmail.com>
2023-04-12 21:42:36 +0700

    create UserDAO
    get write query get information in database

---
+++ DormManager/src/java/controller/Users/Register.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.Users;
+
+import dal.UsersDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Date;
+import modol.Users;
+
+/**
+ *
+ * @author ADMIN
+ */
+
+public class Register extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Register</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Register at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        request.getRequestDispatcher("view/register.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String full_name = request.getParameter("displayname");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String repassword = request.getParameter("repassword");
+        String gender = request.getParameter("gender");
+        String dob = request.getParameter("dob");
+        String phone = request.getParameter("phone");
+        if (!password.equals(repassword)) {
+            request.setAttribute("registerFailed", "Register Unsuccessfully");
+            request.getRequestDispatcher("view/register.jsp").forward(request, response);
+            
+        } else {
+            UsersDAO db = new UsersDAO();
+            Users account = db.checkAccountExist(username);
+            if (account == null) {
+                Users u = new Users();
+                u.setUsername(username);
+                u.setPassword(password);
+                u.setFull_name(full_name);
+                u.setGender(gender.equals("male"));          
+                u.setDob(Date.valueOf(dob));
+                u.setPhone(phone);
+                u.setRole_id(2);
+                db.createAccount(u);  
+                
+                response.sendRedirect("login");
+//                request.getRequestDispatcher("view/login.jsp").forward(request, response);
+                
+            } // account already exist in DB
+            else {
+                response.sendRedirect("register");
+            }
+
+        }
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ DormManager/src/java/dal/UsersDAO.java
@@ -0,0 +1,256 @@
+package dal;
+
+import context.DBContext;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modol.Users;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class UsersDAO extends DBContext {
+    public void change(Users u){
+        String sql = "update Users set password = ? where username=?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, u.getPassword());
+            ps.setString(2, u.getUsername());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+    public ArrayList<Users> getListByPage(ArrayList<Users> list,
+            int start, int end) {
+        ArrayList<Users> arr = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            arr.add(list.get(i));
+        }
+        return arr;
+    }
+
+    public Users getUsers(String user, String pass) {
+        Users account = null;
+        try {
+            String sql = "SELECT * FROM Users\n"
+                    + "                     WHERE username = ?  AND password = ?";
+            //Step2: create obj PrepareStatement
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            //Step 3: thuc thti truy van
+            ResultSet rs = ps.executeQuery();
+            //Step 4: xu ly kq tra  ve
+            while (rs.next()) {
+                //lay du lieu tu rs gan cho cac bien cuc bo
+                account = new Users(rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getInt("user_id"),
+                        rs.getString("full_name"),
+                        rs.getInt("role_id"),
+                        rs.getBoolean("gender"),
+                        rs.getDate("dob"),
+                        rs.getString("phone"));
+            }
+        } catch (SQLException e) {
+        }
+
+        return account;
+    }
+
+    public Users checkAccountExist(String username) {
+        try {
+            String sql = "select * from Users\n"
+                    + "where [username] = ?\n";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Users account = new Users();
+                account.setUsername(rs.getString("username"));
+                account.setPassword(rs.getString("password"));
+                return account;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public void createAccount(Users u) {
+        try {
+            String sql = "INSERT INTO  [dbo].[Users]\n"
+                    + "           ([username]\n"
+                    + "           ,[password]\n"
+                    + "           ,[full_name]\n"
+                    + "           ,[role_id]\n"
+                    + "           ,[gender]\n"
+                    + "           ,[dob]\n"
+                    + "           ,[phone]\n)"
+                    + "     VALUES\n"
+                    + "           (?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?\n"
+                    + "           ,?)";
+
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, u.getUsername());
+            stm.setString(2, u.getPassword());
+            stm.setString(3, u.getFull_name());
+            stm.setInt(4, u.getRole_id());
+            stm.setBoolean(5, u.isGender());
+            stm.setDate(6, u.getDob());
+            stm.setString(7, u.getPhone());
+            stm.executeUpdate();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ArrayList<Users> getAllUser() {
+        ArrayList<Users> user = new ArrayList<>();
+        try {
+            String sql = "SELECT [user_id]\n"
+                    + "      ,[username]\n"
+                    + "      ,[full_name]\n"
+                    + "      ,[role_id]\n"
+                    + "      ,[gender]\n"
+                    + "      ,[dob]\n"
+                    + "      ,[phone]\n"
+                    + "  FROM [dbo].[Users]";
+            //Step2: create obj PrepareStatement
+            PreparedStatement ps = connection.prepareStatement(sql);
+            //Step 3: thuc thti truy van
+            ResultSet rs = ps.executeQuery();
+            //Step 4: xu ly kq tra  ve
+            while (rs.next()) {
+                //lay du lieu tu rs gan cho cac bien cuc bo
+
+                Users u = new Users();
+                u.setUser_id(rs.getInt("user_id"));
+                u.setUsername(rs.getString("username"));
+                u.setFull_name(rs.getString("full_name"));
+                u.setRole_id(rs.getInt("role_id"));
+                u.setGender(rs.getBoolean("gender"));
+                u.setDob(rs.getDate("dob"));
+                u.setPhone(rs.getString("phone"));
+                user.add(u);
+            }
+        } catch (SQLException e) {
+        }
+
+        return user;
+    }
+
+    public void update(Users user) {
+        PreparedStatement stm = null;
+        try {
+            String sql = "UPDATE [dbo].[Users]\n"
+                    + "   SET [role_id] = ?\n"
+                    + " WHERE user_id = ?";
+            stm = connection.prepareStatement(sql);
+            stm.setInt(1, user.getRole_id());
+            stm.setInt(2, user.getUser_id());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                stm.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void updateUser(Users user) {
+        PreparedStatement stm = null;
+        try {
+            String sql = "UPDATE [dbo].[Users]\n"
+                    + "   SET [full_name] = ?\n"
+                    + "      ,[gender] = ?\n"
+                    + "      ,[dob] = ?\n"
+                    + "      ,[phone] = ? \n"
+                    + " WHERE user_id = ?";
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, user.getFull_name());
+            stm.setBoolean(2, user.isGender());
+            stm.setDate(3, user.getDob());
+            stm.setString(4, user.getPhone());
+            stm.setInt(5, user.getUser_id());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                stm.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void delete(String id) {
+        PreparedStatement stm = null;
+        try {
+            String sql = "DELETE from Users WHERE [user_id] = ?";
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, id);
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Users get(int id) {
+        try {
+            String sql = "SELECT *\n"
+                    + "FROM Users \n"
+                    + "WHERE [user_id] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Users u = new Users();
+                u.setUser_id(rs.getInt("user_id"));
+                u.setUsername(rs.getString("username"));
+                u.setPassword(rs.getString("password"));
+                u.setFull_name(rs.getString("full_name"));
+                u.setRole_id(rs.getInt("role_id"));
+                u.setGender(rs.getBoolean("gender"));
+                u.setDob(rs.getDate("dob"));
+                u.setPhone(rs.getString("phone"));
+
+                return u;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new UsersDAO().get(1));
+    }
+}
